701c676d24c0c78a38c7ee16e3e28bba8bcdfee0
Austin <austinking@outlook.com>
2017-12-24 02:05:16 -0500

    db testing

--- MtgApi/TestMtgApi/Program.cs
+++ MtgApi/TestMtgApi/Program.cs
@@ -34,13 +34,34 @@ namespace MtgApi
                     if (data != null)
                     {
                         JObject parsed = JObject.Parse(data);
+                        var myConnString = @"server = Austin; uid = root; pwd = default; database = magicfinancedb";
+                        MySqlConnection connection = new MySqlConnection(myConnString);
 
                         foreach (var obj in parsed["data"])
                         {
                             string name = (string)obj["name"];
-                            string price = (string)obj["usd"];
+                            string usd = (string)obj["usd"];
+                            string set = (string)obj["set"];
+                            string set_name = (string)obj["set_name"];
 
-                            Console.WriteLine("{0}: ${1}", name, price);
+                            Console.WriteLine("{2} - {0}: ${1}", name, usd, set);
+
+                            connection.Open();
+                            try
+                            {
+                                var SqlCommandString = "INSERT INTO Card (name, `set`, set_name, usd) VALUES (@name, @set, @set_name, @usd);";
+                                MySqlCommand cmd = new MySqlCommand(SqlCommandString, connection);
+                                cmd.Parameters.AddWithValue("@name", name);
+                                cmd.Parameters.AddWithValue("@usd", usd);
+                                cmd.Parameters.AddWithValue("@set_name", set_name);
+                                cmd.Parameters.AddWithValue("@set", set);
+                                cmd.ExecuteNonQuery();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                            }
+                            connection.Close();
                         }
 
                         if (parsed["has_more"].ToString().ToLower() == "true")
@@ -56,28 +77,28 @@ namespace MtgApi
                 }
                 Thread.Sleep(100);
 
-                var myConnString = @"server = 192.168.1.140; uid = root; pwd = default; database = magicfinancedb";
-                MySqlConnection connection = new MySqlConnection(myConnString);
+                //var myConnString = @"server = 192.168.1.140; uid = root; pwd = default; database = magicfinancedb";
+                //MySqlConnection connection = new MySqlConnection(myConnString);
 
-                try
-                {
-                    MySqlCommand cmd = new MySqlCommand("SELECT * FROM TestTable;", connection);
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
-                    MySqlDataReader reader = cmd.ExecuteReader();
+                //try
+                //{
+                //    MySqlCommand cmd = new MySqlCommand("SELECT * FROM TestTable;", connection);
+                //    connection.Open();
+                //    cmd.ExecuteNonQuery();
+                //    MySqlDataReader reader = cmd.ExecuteReader();
 
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("{0}: {1}",
-                            reader.GetInt32(0),
-                            reader.GetValue(1));
-                    }
-                    connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+                //    while (reader.Read())
+                //    {
+                //        Console.WriteLine("{0}: {1}",
+                //            reader.GetInt32(0),
+                //            reader.GetValue(1));
+                //    }
+                //    connection.Close();
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex);
+                //}
             }
 
         }
